c61fa18313813ec88aa01a1bbd09776371f2296a
Lauty Lombardo <lauty.lombardo@hotmail.com>
2025-06-19 15:31:48 -0300

    Segundo Commit - Primer GET con migrations

--- PrimeraAPI/Models/Album.cs
+++ PrimeraAPI/Models/Album.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string AlbumType { get; set; } // Studio, Live, EP, etc.
+        public DateTime Date { get; set; }
 
         public int BandId { get; set; }
         public Band Band { get; set; }
--- PrimeraAPI/Models/Band.cs
+++ PrimeraAPI/Models/Band.cs
@@ -4,9 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string CountryOfOrigin { get; set; }
-        public int YearFormed { get; set; }
+        public string Country { get; set; }
         public string Genre { get; set; }
+        public int Year { get; set; }
+
+        public bool Active { get; set; }
 
         public List<Album> Albums { get; set; }
         public List<Member> Members { get; set; }
